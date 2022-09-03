43cec5063d31718b186088d555e49955df5d9152
Ocampo <mariaocampo0513@gmail.com>
2022-09-03 14:47:14 -0700

    added hooks and on-click event to navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,6 +6,11 @@ import './navbar.scss';
 
 
 export const Navbar = () => {
+
+    const [isActive, setisActive] = React.useState(false)
+
+
+
     return (
         <nav className='navbar' role='navigation' aria-label='main navigation'>
         <div className='navbar-brand'>
@@ -18,19 +23,22 @@ export const Navbar = () => {
           </a>
   
           <a
-            role='button'
-            className={'navbar-burger burger'}
-            aria-label='menu'
-            aria-expanded='false'
-            data-target='navbarBasicExample'
-          >
+          onClick={() => {
+            setisActive(!isActive)
+          }}
+          role='button'
+          className={`navbar-burger burger ${isActive ? 'is-active' : ''}`}
+          aria-label='menu'
+          aria-expanded='false'
+          data-target='navbarBasicExample'
+        >
             <span aria-hidden='true'></span>
             <span aria-hidden='true'></span>
             <span aria-hidden='true'></span>
           </a>
         </div>
-        <div id='navbarBasicExample' className='navbar-menu'>
-          <div className='navbar-end'>
+        <div id='navbarBasicExample' className={`navbar-menu ${isActive ? 'is-active' : ''}`}>
+        <div className='navbar-end'>
             <div className='navbar-item'>
               <a href='/' className='navbar-item'>
                 Home
