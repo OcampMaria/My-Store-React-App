542fc5271683f98f5721c0676c82fce048c5505c
Ocampo <mariaocampo0513@gmail.com>
2022-09-03 14:01:29 -0700

    styled navbar with React Bulma CSS framework

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import {Navbar} from './components/navbar/Navbar';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bulma/css/bulma.min.css';
 
 function App() {
   return (
     <div className="App">    
       <Navbar/>
+      
     </div>
+    
   );
 }
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,18 +1,49 @@
 import React from 'react';
+import orangeLogo from '../../Assets/orange.png';
+
 import './navbar.scss';
-import {Icon} from '@iconify/react';
+// import {Icon} from '@iconify/react';
 
 
 export const Navbar = () => {
     return (
-      <header>
-        <div className="navbar">
-           <Icon className="navbar_icon" icon='logos:react'/>
-           <h1 className="navbar_title">Clase2</h1>
-           <Icon className="navbar_icon" icon='material-symbols:accessibility-new' color='#61dbfb' />
-
+        <nav className='navbar' role='navigation' aria-label='main navigation'>
+        <div className='navbar-brand'>
+          <a href='/' className='navbar-item'>
+            <img
+              src={orangeLogo}
+              alt='Logo'
+              
+            />
+          </a>
+  
+          <a
+            role='button'
+            className={'navbar-burger burger'}
+            aria-label='menu'
+            aria-expanded='false'
+            data-target='navbarBasicExample'
+          >
+            <span aria-hidden='true'></span>
+            <span aria-hidden='true'></span>
+            <span aria-hidden='true'></span>
+          </a>
         </div>
-      </header>
+        <div id='navbarBasicExample' className='navbar-menu'>
+          <div className='navbar-end'>
+            <div className='navbar-item'>
+              <a href='/' className='navbar-item'>
+                Home
+              </a>
+              <a href='/' className='navbar-item'>
+                Documentation
+              </a>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      
     )
 }
 
